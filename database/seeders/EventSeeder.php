2f948024bb5994ef4deee2ffeb8895dbda1f5276
Kundan <kundanborichak@gmail.com>
2025-06-22 21:37:32 +0530

    seeder set for check

--- database/seeders/EventSeeder.php
+++ database/seeders/EventSeeder.php
@@ -10,7 +10,6 @@ class EventSeeder extends Seeder
 {
     public function run()
     {
-        // 🔥 Indian-style event names
         $titles = [
             'Wedding Ceremony',
             'Reception Party',
@@ -34,11 +33,47 @@ class EventSeeder extends Seeder
             'Community Meetup'
         ];
 
+        $descriptions = [
+            'This is a special event for close family and friends.',
+            'Don’t miss out on the delicious food and music!',
+            'Important discussion with key stakeholders.',
+            'Routine checkup and follow-up appointment.',
+            'Celebrate the festival with joy and togetherness.',
+            'A religious ceremony with community gathering.',
+            'A fun party with lots of entertainment.',
+            'An official meeting with important presentations.',
+            'Catch up with old friends and relive memories.',
+            'An auspicious day with rituals and blessings.',
+        ];
+
+        $locations = [
+            'SG Highway, Ahmedabad',
+            'Maninagar, Ahmedabad',
+            'Law Garden, Ahmedabad',
+            'Bodakdev, Ahmedabad',
+            'Satellite, Ahmedabad',
+            'Vastrapur, Ahmedabad',
+            'Navrangpura, Ahmedabad',
+            'Ellis Bridge, Ahmedabad',
+            'Chandkheda, Ahmedabad',
+            'Gota, Ahmedabad',
+        ];
+
+        // Helper for random time between 9 AM to 8 PM
+        $getRandomTime = function () {
+            $hour = rand(9, 20); // 9 AM to 8 PM
+            $minute = rand(0, 59);
+            return sprintf('%02d:%02d:00', $hour, $minute);
+        };
+
         // ✅ TODAY'S EVENTS
         for ($i = 0; $i < 5; $i++) {
             Event::create([
                 'title' => $titles[array_rand($titles)],
+                'description' => $descriptions[array_rand($descriptions)],
                 'date' => Carbon::today()->toDateString(),
+                'time' => $getRandomTime(),
+                'location' => $locations[array_rand($locations)],
             ]);
         }
 
@@ -46,7 +81,10 @@ class EventSeeder extends Seeder
         for ($i = 0; $i < 5; $i++) {
             Event::create([
                 'title' => $titles[array_rand($titles)],
+                'description' => $descriptions[array_rand($descriptions)],
                 'date' => Carbon::today()->addDays(rand(1, 30))->toDateString(),
+                'time' => $getRandomTime(),
+                'location' => $locations[array_rand($locations)],
             ]);
         }
 
@@ -54,7 +92,10 @@ class EventSeeder extends Seeder
         for ($i = 0; $i < 5; $i++) {
             Event::create([
                 'title' => $titles[array_rand($titles)],
+                'description' => $descriptions[array_rand($descriptions)],
                 'date' => Carbon::today()->subDays(rand(1, 30))->toDateString(),
+                'time' => $getRandomTime(),
+                'location' => $locations[array_rand($locations)],
             ]);
         }
     }
